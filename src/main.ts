759e3202fd44edadb91f38aea81985594438cb99
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2025-02-26 18:23:05 -0800

    App Running - CheckPoint

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { bootstrapApplication, provideClientHydration } from '@angular/platform-
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 // Factory function for ngx-translate Http loader
