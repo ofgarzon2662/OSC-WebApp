62725e7bd091dbccc66e5d87c71facfe8d0fbce4
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2025-02-26 16:00:12 -0800

    Initial modules and componentes

---
+++ src/app/artifact/list-artifacst/list-artifacst.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-artifacst',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-artifacst.component.html',
+  styleUrl: './list-artifacst.component.css'
+})
+export class ListArtifacstComponent {
+
+}
---
+++ src/app/home/artifact-preview/artifact-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artifact-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './artifact-preview.component.html',
+  styleUrl: './artifact-preview.component.css'
+})
+export class ArtifactPreviewComponent {
+
+}
